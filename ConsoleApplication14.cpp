5e0ed77ab4595a435beaf3442eff3beae9a4711b
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-02-24 19:05:22 +0200

    Add files via upload

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string Compress(const string& str) 
+{
+    string NewText;
+    bool space = true;
+
+    for (int i = 0; i < str.length(); i++) 
+    {
+        if (isalpha(str[i])) 
+        {
+            NewText.push_back(str[i]);
+            space = false;
+        }
+        else if (str[i] == ' ' && space) 
+        {
+            NewText.push_back(str[i]);
+        }
+        else if (str[i] == ' ' && !space) 
+        {
+            if (!isspace(NewText.back())) 
+            {
+                NewText.push_back(str[i]);
+            }
+        }
+    }
+    return NewText;
+}
+
+int main() {
+    string Text;
+    getline(cin,Text);
+
+   cout << Compress(Text);
+   
+}
